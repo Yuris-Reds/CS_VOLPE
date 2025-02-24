b3984df0d2bed9ee6dd60e1fbd158c89b75110a8
emagent006 <119740257+emagent006@users.noreply.github.com>
2025-02-24 09:48:02 +0100

    ddlAdj

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Presidente(
-    cf varchar(16) not null,
+CREATE TABLE Presidenti(
+    cf char(16) not null,
     nome varchar(25) not null,
     cognome varchar(25) not null,
 
@@ -7,57 +7,65 @@ CREATE TABLE Presidente(
 )ENGINE=InnoDB;
 
 CREATE TABLE Societa(
-    id integer unsigned auto_increment,
-    nome varchar(30) not null,
-    comune varchar(30) not null,
+    id integer unsigned auto_increment not null,
+    nome varchar(20) not null,
+    comune varchar(20) not null,
     provincia char(2) not null,
-    indirizzo varchar(30) not null,
+    cf_presidente char(16) not null,
+
+    primary key(id),
+    foreign key(cf_presidente) references Presidenti(cf)
+)ENGINE=InnoDB;
+
+CREATE TABLE Allenatori(
+    cf char(16) not null,
+    nome varchar(20) not null,
+    cognome varchar(20) not null,
+    id_squadra integer unsigned,
+
+    primary key(cf),
+    foreign key(id_squadra) references Squadre(id)
+)ENGINE=InnoDB;
+
+--Un dirigente appartiene solo a una societa':
+CREATE TABLE Dirigenti(
+    cf char(16) not null,
+    nome varchar(20) not null,
+    cognome varchar(20) not null,
+    id_societa integer unsigned,
+    
+    primary key(cf),
+    foreign key(id_societa) references Societa(id)
+)ENGINE=InnoDB;
+
+--Una societa' partecipa ad un solo campionato ed e' posseduta da una sola societa':
+CREATE TABLE Squadre(
+    id integer unsigned auto_increment not null,
+    nome varchar(20) not null,
+    id_campionato integer unsigned not null,
+    id_societa integer unsigned not null,
+
+    primary key(id),
+    foreign key(id_campionato) references Campionati(id),
+    foreign key(id_societa) references Societa(id)
+)ENGINE=InnoDB;
+
+CREATE TABLE Campionati(
+    id integer unsigned auto_increment not null,
+    nome varchar(30) not null,
+    genere char(1) not null,
+    anno integer unsigned not null,
 
     primary key(id)
 )ENGINE=InnoDB;
 
-CREATE TABLE Allenatori(
-    cf varchar(16) not null,
-    nome varchar(25) not null,
-    cognome varchar(25) not null,
-    squadra varchar(30) not null,
-
-    primary key(cf),
-    foreign key(squadra) references Squadra(id),
-)ENGINE=InnoDB;
-
-CREATE TABLE Dirigenti(
-    cf varchar(16) not null,
-    nome varchar(25) not null,
-    cognome varchar(25) not null,
-
-    primary key(cf),
-)ENGINE=InnoDB;
-
-CREATE TABLE Squadra(
-    id integer unsigned auto_increment,
-    campionato varchar(40) not null,
-    nome varchar(25) not null,
-
-    primary key(id),
-    foreign key(campionato) references Campionato(id),
-)ENGINE=InnoDB;
-
-
-CREATE TABLE Campionato(
-    id integer unsigned auto_increment,
-    nome varchar(40) not null,
-    genere char(1) not null,
-    anno integer not null,
-
-    primary key(id),
-)ENGINE=InnoDB;
-
 CREATE TABLE Atleti(
     matricola integer unsigned auto_increment,
-    nome varchar(25) not null,
-    cognome varchar(25) not null,
+    nome varchar(20) not null,
+    cognome varchar(20) not null,
     dataNascita date not null,
+    id_squadra integer unsigned not null,
 
     primary key(matricola),
+    foreign key(id_squadra) references Squadre(id)
 )ENGINE=InnoDB;

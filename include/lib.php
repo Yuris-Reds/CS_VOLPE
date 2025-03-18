05d69f5fd91940aecac4c215adf0fc5b33208257
emagent006 <emanuele.gentile@galileiostiglia.edu.it>
2025-03-18 08:06:25 +0100

    s

--- include/lib.php
+++ include/lib.php
@@ -2,7 +2,7 @@
 
 $DBHOST = "localhost";
 $DBUSER = "root";
-$DBPASSWORD = "root"; //!
+$DBPASSWORD = ""; //!
 $DBNAME = "cs_volpe";
 
 function writeHeader(){
@@ -78,7 +78,7 @@ function writeMenu(){
     echo('
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="index.php"><img src="../IT-CONI.png" width="15%"></a>
+                <a class="navbar-brand" href="index.php"><img src="../IT-CONI.png" width="30px"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
